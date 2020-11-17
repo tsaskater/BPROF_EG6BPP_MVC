d021b35fe552b067139b7aefea43da90d7efe66a
tsaskater <parasztragu@gmail.com>
2020-11-17 14:14:24 +0100

    Startup mvc hozzáadva

--- Feleves/Feleves/Startup.cs
+++ Feleves/Feleves/Startup.cs
@@ -16,6 +16,7 @@ namespace Feleves
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc(opt => opt.EnableEndpointRouting = false);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,15 +27,8 @@ namespace Feleves
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseMvcWithDefaultRoute();
             app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
-            });
         }
     }
 }
