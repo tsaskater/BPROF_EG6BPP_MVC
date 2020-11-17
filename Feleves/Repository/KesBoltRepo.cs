488b266f13b24932aa9374f01bdb40070ea54414
tsaskater <parasztragu@gmail.com>
2020-11-17 16:28:23 +0100

    KesRepo és KesBolt Repo elkészítve

---
+++ Feleves/Repository/KesBoltRepo.cs
@@ -0,0 +1,57 @@
+﻿using Data;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Repository
+{
+    public class KesBoltRepo : IRepository<Kes_Bolt_Keszlet_Info>
+    {
+        VelemenyContextDb db = new VelemenyContextDb();
+
+        public void Add(Kes_Bolt_Keszlet_Info kesbolt)
+        {
+            db.Kes_Bolt.Add(kesbolt);
+            db.SaveChanges();
+        }
+
+        public void Delete(string kbId)
+        {
+            Delete(Read(kbId));
+        }
+
+        public void Delete(Kes_Bolt_Keszlet_Info kb)
+        {
+            db.Kes_Bolt.Remove(kb);
+            db.SaveChanges();
+        }
+
+        public Kes_Bolt_Keszlet_Info Read(string Kbid)
+        {
+            return db.Kes_Bolt.FirstOrDefault(tmp => tmp.Raktar_Id == Kbid);
+        }
+
+        public IQueryable<Kes_Bolt_Keszlet_Info> Read()
+        {
+            return db.Kes_Bolt.AsQueryable();
+        }
+
+        public void SaveAll()
+        {
+            db.SaveChanges();
+        }
+
+        public void Update(string o_KbId, Kes_Bolt_Keszlet_Info N_kb)
+        {
+            var Old_Kb = Read(o_KbId);
+
+            Old_Kb.Bolt_Nev = N_kb.Bolt_Nev;
+            Old_Kb.Cim = N_kb.Cim;
+            Old_Kb.Weboldal = N_kb.Weboldal;
+            SaveAll();
+        }
+    }
+
+}
---
+++ Feleves/Repository/KesRepo.cs
@@ -0,0 +1,60 @@
+﻿using Data;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Repository
+{
+    public class KesRepo : IRepository<Kes>
+    {
+        VelemenyContextDb db = new VelemenyContextDb();
+
+        public void Add(Kes kes)
+        {
+            db.Kesek.Add(kes);
+            db.SaveChanges();
+        }
+
+        public void Delete(string kId)
+        {
+            Delete(Read(kId));
+        }
+
+        public void Delete(Kes kes)
+        {
+            db.Kesek.Remove(kes);
+            db.SaveChanges();
+        }
+
+        public Kes Read(string Kid)
+        {
+            return db.Kesek.FirstOrDefault(tmp => tmp.Gyartasi_Cikkszam == Kid);
+        }
+
+        public IQueryable<Kes> Read()
+        {
+            return db.Kesek.AsQueryable();
+        }
+
+        public void SaveAll()
+        {
+            db.SaveChanges();
+        }
+
+        public void Update(string o_KId, Kes N_kes)
+        {
+            var Old_Kes = Read(o_KId);
+
+            Old_Kes.Gyarto = N_kes.Gyarto;
+            Old_Kes.Modell_nev = N_kes.Modell_nev;
+            Old_Kes.Markolat = N_kes.Markolat;
+            Old_Kes.Bevont_Penge= N_kes.Bevont_Penge;
+            Old_Kes.Penge_Hossz= N_kes.Penge_Hossz;
+            Old_Kes.Acel = N_kes.Acel;
+            Old_Kes.Ar = N_kes.Ar;
+            SaveAll();
+        }
+    }
+}
