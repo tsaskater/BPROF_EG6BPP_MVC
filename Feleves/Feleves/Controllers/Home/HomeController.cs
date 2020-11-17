19e930156358f8f8c4c7f897d77c59d4ff1fcfdc
tsaskater <parasztragu@gmail.com>
2020-11-17 17:01:41 +0100

    Home controller létrehozva

---
+++ Feleves/Feleves/Controllers/Home/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Logic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Feleves.Controllers.Home
+{
+    public class HomeController : Controller
+    {
+        VelemenyLogic Velemenylogic;
+        KesLogic Keslogic;
+        KesBoltLogic KesBoltlogic;
+
+        public HomeController(VelemenyLogic Velemenylogic, KesLogic Keslogic, KesBoltLogic KesBoltlogic)
+        {
+            this.Velemenylogic = Velemenylogic;
+            this.Keslogic = Keslogic;
+            this.KesBoltlogic = KesBoltlogic;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
