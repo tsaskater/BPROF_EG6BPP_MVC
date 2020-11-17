c34ed12bd4be57d280b271d1dc50d2ead19c0b7c
tsaskater <parasztragu@gmail.com>
2020-11-17 15:51:40 +0100

    Repository library hozzáadva

---
+++ Feleves/Repository/IRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Repository
+{
+
+    public interface IRepository<T> where T : new()
+    {
+        void Add(T item);
+
+        void Delete(T item);
+
+        void Delete(string id);
+
+        T Read(string id);
+
+        IQueryable<T> Read();
+
+        void Update(string oid, T NItem);
+
+        void Save();
+    }
+}
